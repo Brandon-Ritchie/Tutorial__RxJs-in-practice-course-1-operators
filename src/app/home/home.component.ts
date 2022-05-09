1211cda82881f6e11d6f68e0b4bf9973e4071df8
Brandon Ritchie <britchie@avi>
2022-05-09 13:21:54 -0400

    Building Components with RxJs - Reactive Design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,30 +17,27 @@ import { createHttpObservable } from "../common/util";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
   ngOnInit() {
     const http$ = createHttpObservable("/api/courses");
 
-    const courses$ = http$.pipe(
-      map((res) => Object.values<Course>(res["payload"]))
+    const courses$: Observable<Course[]> = http$.pipe(
+      map((res) => Object.values(res["payload"]))
     );
 
-    courses$.subscribe(
-      (courses) => {
-        this.beginnerCourses = courses.filter(
-          (course) => course.category == "BEGINNER"
-        );
-        this.advancedCourses = courses.filter(
-          (course) => course.category == "ADVANCED"
-        );
-      },
-      noop,
-      () => {
-        console.log("completed");
-      }
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
+
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
     );
   }
 }
