9a22f39802644982fa51b9761d18203d9e50c1ec
Brandon Ritchie <britchie@avi>
2022-05-10 09:57:40 -0400

    Final section of 1-Operators

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -21,16 +21,5 @@ import { createHttpObservable } from "../common/util";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    const subject = new Subject();
-
-    const series$ = subject.asObservable();
-
-    series$.subscribe(console.log);
-
-    subject.next(1);
-    subject.next(2);
-    subject.next(3);
-    subject.complete();
-  }
+  ngOnInit() {}
 }
